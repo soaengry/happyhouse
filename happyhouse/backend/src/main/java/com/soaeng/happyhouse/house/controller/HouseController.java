71621238711770d8104e5c4d369f7cc01bd4c128
soaeng <sodud0695@naver.com>
2025-10-12 20:55:49 +0900

    [fix]

--- happyhouse/backend/src/main/java/com/soaeng/happyhouse/house/controller/HouseController.java
+++ happyhouse/backend/src/main/java/com/soaeng/happyhouse/house/controller/HouseController.java
@@ -1,15 +1,20 @@
 package com.soaeng.happyhouse.house.controller;
 
 import com.soaeng.happyhouse.house.dto.request.HouseParamDto;
+import com.soaeng.happyhouse.house.dto.response.HouseDto;
 import com.soaeng.happyhouse.house.dto.response.HouseResponseDto;
 import com.soaeng.happyhouse.house.service.HouseService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @Slf4j
@@ -24,7 +29,8 @@ public class HouseController {
     private static final int FAIL = -1;
 
     @GetMapping("")
-    public ResponseEntity<HouseResponseDto> getHouseList(@RequestBody HouseParamDto param) {
+    public ResponseEntity<HouseResponseDto> getHouseList(HouseParamDto param) {
+        log.info(param.toString());
         HouseResponseDto response = new HouseResponseDto();
 
         if (param.getKeyword() != null) {
@@ -57,6 +63,7 @@ public class HouseController {
         } else {
             if (param.getDongCode() > 0) {
                 response.setHouseList(service.getDongDealList(param));
+                log.info(response.getHouseList().toString());
                 response.setCount(service.getDongDealCount(param.getDongCode()));
             } else if (param.getGugunCode() > 0) {
                 response.setHouseList(service.getGugunDealList(param));
@@ -65,7 +72,10 @@ public class HouseController {
                 response.setHouseList(service.getSidoDealList(param));
                 response.setCount(service.getSidoDealCount(param.getSidoCode()));
             } else {
-                response.setHouseList(service.getAllDealList(param));
+                List<HouseDto> houseList = service.getAllDealList(param);
+                log.info(service.getAllDealList(param).toString());
+                response.setHouseList(houseList);
+                log.info(response.getHouseList().toString());
                 response.setCount(service.getAllDealCount());
             }
         }
