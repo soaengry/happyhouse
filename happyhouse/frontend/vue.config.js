661ed62c5caa1b945b2e3993a4d16a7791f2fc18
soaeng <sodud0695@naver.com>
2025-10-15 20:35:46 +0900

    [chore] ws 주소 설정

--- happyhouse/frontend/vue.config.js
+++ happyhouse/frontend/vue.config.js
@@ -2,6 +2,10 @@ const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
   devServer: {
+    port: 3000,
+    client: {
+      webSocketURL: "ws://localhost:3000/ws",
+    },
     proxy: {
       "/api": {
         target: "http://localhost:8080",
